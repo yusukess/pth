b0cb151de4d43a69968488245be996c57a9223f4
yusukess <yusukes@me.com>
2023-11-21 00:14:01 +0900

    landscape時の表示調整

--- src/js/index.js
+++ src/js/index.js
@@ -1,98 +1,108 @@
-(function($){$(function(){
+(function ($) {
+  $(function () {
     /*====================
      *
      * slickスライダー
      *
      ====================*/
     // top > newsのスライダー
-    $(".slider-multiple").slick({
-        infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<button class="arrow-prev"></button>',
-        nextArrow: '<button class="arrow-next"></button>',
-        responsive: [{
-            breakpoint: 415,
-            settings: "unslick"
-        }]
+    $('.slider-multiple').slick({
+      infinite: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<button class="arrow-prev"></button>',
+      nextArrow: '<button class="arrow-next"></button>',
+      responsive: [
+        {
+          breakpoint: 430,
+          settings: 'unslick',
+        },
+      ],
     });
 
     // top > キービジュアルのスライダー
-    $(".slider-kv").slick({
-        infinite: true,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        fade: true,
-        speed: 1800,
-        responsive: [{
-            breakpoint: 415,
-            settings: {
-                dots: false
-            }
-        }]
+    $('.slider-kv').slick({
+      infinite: true,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      fade: true,
+      speed: 1800,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      pauseOnDotsHover: false,
+      waitForAnimate: false,
+      responsive: [
+        {
+          breakpoint: 430,
+          settings: {
+            dots: false,
+          },
+        },
+      ],
+    });
+
+    // SPでスライダーが止まるのを防止
+    $('.slider-kv').on('touchmove', function (event, slick, currentSlide, nextSlide) {
+      $('.slider-kv').slick('slickPlay');
     });
 
     // ギャラリースライダー（共有施設・ギャラリー）
     let slider_photo_setting_base = {
-        infinite: false,
-        asNavFor: '.slider-gallery_nav',
-        prevArrow: '<button class="arrow-prev"></button>',
-        nextArrow: '<button class="arrow-next"></button>',
+      infinite: false,
+      asNavFor: '.slider-gallery_nav',
+      prevArrow: '<button class="arrow-prev"></button>',
+      nextArrow: '<button class="arrow-next"></button>',
     };
 
     // ギャラリースライダーのナビゲーションスライダー（共有施設・ギャラリー）
     let slider_photo_nav_setting_base = {
-        infinite: false,
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        asNavFor: '.slider-photo',
-        focusOnSelect: true,
-        swipeToSlide: true,
-        arrows: false
+      infinite: false,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      asNavFor: '.slider-photo',
+      focusOnSelect: true,
+      swipeToSlide: true,
+      arrows: false,
     };
 
     const common_facilities_area = [
-        "entrance",
-        "rounge",
-        "guest-room",
-        "open-garden",
-        "activity-space",
-        "cabin",
-        "owners-living",
-        "kids",
-        "parking",
-        "emergency",
-        "other",
+      'entrance',
+      'rounge',
+      'guest-room',
+      'open-garden',
+      'activity-space',
+      'cabin',
+      'owners-living',
+      'kids',
+      'parking',
+      'emergency',
+      'other',
     ];
 
-    const event = [
-        "tanabata",
-        "halloween",
-        "illumination"
-    ];
+    const event = ['tanabata', 'halloween', 'illumination'];
 
     common_facilities_area.forEach(($target) => {
-        let tmp_setting = {...slider_photo_setting_base};
-        let tmp_nav_setting = {... slider_photo_nav_setting_base};
+      let tmp_setting = { ...slider_photo_setting_base };
+      let tmp_nav_setting = { ...slider_photo_nav_setting_base };
 
-        tmp_setting['asNavFor'] = "." + $target + " .slider-gallery_nav";
-        tmp_nav_setting['asNavFor'] = "." + $target + " .slider-photo";
+      tmp_setting['asNavFor'] = '.' + $target + ' .slider-gallery_nav';
+      tmp_nav_setting['asNavFor'] = '.' + $target + ' .slider-photo';
 
-        $("." + $target + " .slider-photo").slick(tmp_setting);
-        $("." + $target + " .slider-gallery_nav").slick(tmp_nav_setting);
+      $('.' + $target + ' .slider-photo').slick(tmp_setting);
+      $('.' + $target + ' .slider-gallery_nav').slick(tmp_nav_setting);
     });
 
     event.forEach(($target) => {
-        let tmp_setting = {...slider_photo_setting_base};
-        let tmp_nav_setting = {... slider_photo_nav_setting_base};
+      let tmp_setting = { ...slider_photo_setting_base };
+      let tmp_nav_setting = { ...slider_photo_nav_setting_base };
 
-        tmp_setting['asNavFor'] = "." + $target + " .slider-gallery_nav";
-        tmp_nav_setting['asNavFor'] = "." + $target + " .slider-photo";
+      tmp_setting['asNavFor'] = '.' + $target + ' .slider-gallery_nav';
+      tmp_nav_setting['asNavFor'] = '.' + $target + ' .slider-photo';
 
-        $("." + $target + " .slider-photo").slick(tmp_setting);
-        $("." + $target + " .slider-gallery_nav").slick(tmp_nav_setting);
+      $('.' + $target + ' .slider-photo').slick(tmp_setting);
+      $('.' + $target + ' .slider-gallery_nav').slick(tmp_nav_setting);
     });
 
     /*====================
@@ -101,9 +111,9 @@
      *
      ====================*/
     lightbox.option({
-        'alwaysShowNavOnTouchDevices' : true,
-        'disableScrolling': true,
-        'positionFromTop': 100
+      alwaysShowNavOnTouchDevices: true,
+      disableScrolling: true,
+      positionFromTop: 100,
     });
 
     /*====================
@@ -111,24 +121,24 @@
      * ゾーン切り替え処理
      *
      ====================*/
-    let zone_anchor = $(".zone-anchor");
-    let zone_section = $(".zone-section");
+    let zone_anchor = $('.zone-anchor');
+    let zone_section = $('.zone-section');
 
-    zone_anchor.on("click", function($e){
-        // クリックした対象ゾーンを洗い出す
-        target_class_name = $($e.currentTarget).attr("class").slice(12); //zone-anchorを削除
+    zone_anchor.on('click', function ($e) {
+      // クリックした対象ゾーンを洗い出す
+      target_class_name = $($e.currentTarget).attr('class').slice(12); //zone-anchorを削除
 
-        if(target_class_name.indexOf('current') >= 0) {
-            target_class_name = target_class_name.slice(0, -8); //currentを削除
-        }
+      if (target_class_name.indexOf('current') >= 0) {
+        target_class_name = target_class_name.slice(0, -8); //currentを削除
+      }
 
-        // 全てのボタンからcurrentを外す
-        zone_anchor.removeClass("current");
-        zone_section.removeClass("current");
+      // 全てのボタンからcurrentを外す
+      zone_anchor.removeClass('current');
+      zone_section.removeClass('current');
 
-        // 同じゾーンのボタンを全てcurrentにする
-        $(".zone-anchor." + target_class_name).addClass("current");
-        $(".zone-section." + target_class_name).addClass("current");
+      // 同じゾーンのボタンを全てcurrentにする
+      $('.zone-anchor.' + target_class_name).addClass('current');
+      $('.zone-section.' + target_class_name).addClass('current');
     });
 
     /*====================
@@ -136,20 +146,20 @@
      * スムーズスクロール
      *
      ====================*/
-    $('a[href^="#"]').click(function(){
-        // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
-        var adjust = 0;
-        // スクロールの速度（ミリ秒）
-        var speed = 400;
-        // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
-        var href= $(this).attr("href");
-        // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
-        var position = target.offset().top + adjust;
-        // スムーススクロール linear（等速） or swing（変速）
-        $('body,html').animate({scrollTop:position}, speed, 'swing');
-        return false;
+    $('a[href^="#"]').click(function () {
+      // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
+      var adjust = 0;
+      // スクロールの速度（ミリ秒）
+      var speed = 400;
+      // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
+      var href = $(this).attr('href');
+      // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
+      var target = $(href == '#' || href == '' ? 'html' : href);
+      // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
+      var position = target.offset().top + adjust;
+      // スムーススクロール linear（等速） or swing（変速）
+      $('body,html').animate({ scrollTop: position }, speed, 'swing');
+      return false;
     });
 
     /*====================
@@ -157,21 +167,19 @@
      * レイジーロード
      *
      ====================*/
-    $(window).on('scroll', function (){
+    $(window).on('scroll', function () {
+      var elem = $('.concept_point');
+      var isAnimate = 'animation-start';
 
-        var elem = $('.concept_point');
-        var isAnimate = 'animation-start';
+      elem.each(function () {
+        var elemOffset = $(this).offset().top;
+        var scrollPos = $(window).scrollTop();
+        var wh = $(window).height();
 
-        elem.each(function () {
-
-            var elemOffset = $(this).offset().top;
-            var scrollPos = $(window).scrollTop();
-            var wh = $(window).height();
-
-            if(scrollPos > elemOffset - wh + (wh / 2) ){
-                $(this).addClass(isAnimate);
-            }
-        });
+        if (scrollPos > elemOffset - wh + wh / 2) {
+          $(this).addClass(isAnimate);
+        }
+      });
     });
 
     /*====================
@@ -181,10 +189,11 @@
      ====================*/
     const ham = $('.site-header_hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
     const nav = $('.site-header_panel'); //js-navの要素を取得し、変数navに格納
-    ham.on('click', function () { //ハンバーガーメニューをクリックしたら
-        ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-        nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
-        $("body").toggleClass('active');
+    ham.on('click', function () {
+      //ハンバーガーメニューをクリックしたら
+      ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+      nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+      $('html').toggleClass('active');
     });
 
     /*====================
@@ -192,6 +201,5 @@
      * Google map
      *
      ====================*/
-
-
-});})(jQuery);
+  });
+})(jQuery);
