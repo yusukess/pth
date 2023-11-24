cd0285b4756765afcfda267aa01ca6427516c07c
yusukess <yusukes@me.com>
2023-11-25 03:47:18 +0900

    TOPの動画を追加

--- components/top/movie.php
+++ components/top/movie.php
@@ -4,6 +4,8 @@
             <small>妖精気分でイマジネーションツアーへ！</small>
             ドローンツアー動画公開中
         </h2>
-        <div class="movie-player"></div>
+        <div class="movie-player">
+            <video controls="" poster="<?php echo (get_template_directory_uri() . '/assets/images/top/poster.jpg') ?>" playsinline="" src="<?php echo (get_template_directory_uri() . '/assets/mov/pth.MP4') ?>" width="640" height="360"></video>
+        </div>
     </div>
 </section>
